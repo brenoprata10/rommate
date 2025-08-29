e18d939bec26855ce2cb51b4aa539150b2fb4d6a
Breno Prata <brenoprata@hotmail.com>
2025-08-29 19:27:28 +0200

    feat: add login form logic

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,16 +1,16 @@
-import {GalleryVerticalEnd} from 'lucide-react'
+import {Gamepad2} from 'lucide-react'
 import {LoginForm} from './components/LoginForm'
 
 export default function Login() {
 	return (
-		<div className='grid min-h-svh lg:grid-cols-2'>
-			<div className='flex flex-col gap-4 p-6 md:p-10'>
+		<div className='grid min-h-svh lg:grid-cols-2 text-primary'>
+			<div className='bg-background flex flex-col gap-4 p-6 md:p-10'>
 				<div className='flex justify-center gap-2 md:justify-start'>
 					<a href='#' className='flex items-center gap-2 font-medium'>
 						<div className='bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md'>
-							<GalleryVerticalEnd className='size-4' />
+							<Gamepad2 className='size-4' />
 						</div>
-						Acme Inc.
+						Rommate
 					</a>
 				</div>
 				<div className='flex flex-1 items-center justify-center'>
@@ -19,12 +19,13 @@ export default function Login() {
 					</div>
 				</div>
 			</div>
-			<div className='bg-muted relative hidden lg:block'>
+			<div className='bg-background relative hidden lg:block'>
 				<img
-					src='/placeholder.svg'
+					src='/hero-dark.png'
 					alt='Image'
 					className='absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale'
 				/>
+				<div className='absolute h-full w-full bg-black opacity-10'>&nbsp;</div>
 			</div>
 		</div>
 	)
