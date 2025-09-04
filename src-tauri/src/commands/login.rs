75d98d1376550c8ad2a947345aeb56feebe45e07
Breno Prata <brenoprata@hotmail.com>
2025-09-04 17:47:38 +0200

    feat: add store plugin and initial logic

--- src-tauri/src/commands/login.rs
+++ src-tauri/src/commands/login.rs
@@ -1,4 +1,6 @@
 use serde::{Serialize, Deserialize};
+use crate::enums::error::Error;
+use tauri_plugin_http::reqwest;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct LoginPayload {
@@ -9,6 +11,27 @@ pub struct LoginPayload {
 }
 
 #[tauri::command]
-pub fn login(payload: LoginPayload) -> LoginPayload {
-    payload
+pub async fn login(payload: LoginPayload) -> Result<(), Error> {
+    let LoginPayload {
+        username,
+        password,
+        server_url,
+    } = payload;
+
+    let client = reqwest::Client::builder()
+        .cookie_store(true)
+        .build()?;
+
+    let response = client
+        .post(format!("{}/api/login", server_url))
+        .basic_auth(username, Some(password))
+        .send().await?;
+
+    // Print cookies received
+    println!("Cookies received:");
+    for cookie in response.cookies() {
+        println!("{} = {}", cookie.name(), cookie.value());
+    }
+
+    Ok(())
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,6 @@
 use commands::login::login;
+use tauri_plugin_store::StoreExt;
+use serde_json::json;
 
 mod commands;
 mod enums;
@@ -6,8 +8,26 @@ mod enums;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            // Create a new store or load the existing one
+            // this also put the store in the app's resource table
+            // so your following calls `store` calls (from both rust and js)
+            // will reuse the same store
+            let store = app.store("store.json")?;
+
+            // Note that values must be serde_json::Value instances,
+            // otherwise, they will not be compatible with the JavaScript bindings.
+            store.set("some-key", json!({ "value": 5 }));
+
+            // Get a value from the store.
+            let value = store.get("some-key").expect("Failed to get value from store");
+            println!("{}", value); // {"value":5}
+
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![login])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
