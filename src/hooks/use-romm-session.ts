1cacad5c5603f290dbc8c8301560843d91654b36
Breno Prata <brenoprata@hotmail.com>
2025-10-05 18:40:01 +0200

    feat: check logged in user when checking if user is authenticated

--- src/hooks/use-romm-session.ts
+++ src/hooks/use-romm-session.ts
@@ -1,6 +1,7 @@
 import {useCallback} from 'react'
 import useStore from './use-store'
 import {useNavigate} from 'react-router'
+import {getLoggedInUser} from '@/utils/http/user'
 
 const ROMM_SESSION_KEY = 'romm_session'
 
@@ -9,8 +10,9 @@ export default function useRommSession() {
 	const navigate = useNavigate()
 
 	const isAuthenticated = useCallback(async () => {
-		const rommSession = await get<string>(ROMM_SESSION_KEY)
-		return Boolean(rommSession)
+		const [rommSession, loggedInUser] = await Promise.all([get<string>(ROMM_SESSION_KEY), getLoggedInUser()])
+
+		return Boolean(rommSession) && loggedInUser.success
 	}, [get])
 
 	const logout = useCallback(async () => {
