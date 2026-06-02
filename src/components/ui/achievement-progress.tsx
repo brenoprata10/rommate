864310fb0648cdc0dc9358c42b08b7d3c353a7a6
Breno Prata <brenoprata@hotmail.com>
2026-06-02 11:02:35 +0200

    feat: add design for retroachievements section list

--- src/components/ui/achievement-progress.tsx
+++ src/components/ui/achievement-progress.tsx
@@ -3,40 +3,69 @@ import useServerUrl from '@/hooks/use-server-url'
 import {RetroAchievement, RetroAchievementsMetadata} from '@/models/retroachievements'
 import clsx from 'clsx'
 import useRetroachievements from '@/hooks/use-retroachievements'
+import {useMemo} from 'react'
 
 export default function AchievementProgress({
+	className,
 	contentClassName,
 	rom,
-	maxLockedAchievementCount,
-	maxUnlockedAchievementCount
+	truncateDescription,
+	maxAchievementCount,
+	randomizeOrder
 }: {
+	className?: string
 	contentClassName?: string
 	rom: {raId: number; mergedRaMetadata: RetroAchievementsMetadata}
-	maxUnlockedAchievementCount?: number
-	maxLockedAchievementCount?: number
+	truncateDescription?: boolean
+	maxAchievementCount?: number
+	randomizeOrder?: boolean
 }) {
+	console.log(rom)
 	const {lockedAchievements, unlockedAchievements, progressPercentage, totalAchievementsCount, userRomAchievements} =
 		useRetroachievements({raId: rom.raId, availableAchievements: rom.mergedRaMetadata})
 
+	const achievements = useMemo(
+		() =>
+			[
+				...unlockedAchievements.map((achievement) => ({...achievement, isLocked: false})),
+				...lockedAchievements.map((achievement) => ({...achievement, isLocked: true}))
+			].sort(() => (randomizeOrder ? Math.random() - 0.5 : 0)),
+		[unlockedAchievements, lockedAchievements, randomizeOrder]
+	)
+
+	if (achievements.length === 0) {
+		return null
+	}
+
 	return (
-		<div className='flex flex-col gap-2'>
+		<div className={clsx('flex flex-col gap-2', className)}>
 			<RetroachievementProgress value={progressPercentage} />
 			<span className='text-sm font-medium text-neutral-400 self-end'>
 				{userRomAchievements?.numAwarded ?? 0}/{totalAchievementsCount} Unlocked
 			</span>
-			<div className={clsx(['gap-3', contentClassName])}>
-				{unlockedAchievements.slice(0, maxUnlockedAchievementCount).map((achievement, index) => (
-					<Achievement key={`${achievement.ra_id}-${index}`} achievement={achievement} />
-				))}
-				{lockedAchievements.slice(0, maxLockedAchievementCount).map((achievement, index) => (
-					<Achievement key={`${achievement.ra_id}-${index}`} isLocked achievement={achievement} />
+			<div className={clsx(['flex gap-3', contentClassName])}>
+				{achievements.slice(0, maxAchievementCount).map((achievement) => (
+					<Achievement
+						key={achievement.ra_id}
+						achievement={achievement}
+						truncateDescription={truncateDescription}
+						isLocked={achievement.isLocked}
+					/>
 				))}
 			</div>
 		</div>
 	)
 }
 
-const Achievement = ({achievement, isLocked}: {achievement: RetroAchievement; isLocked?: boolean}) => {
+const Achievement = ({
+	achievement,
+	isLocked,
+	truncateDescription
+}: {
+	achievement: RetroAchievement
+	isLocked?: boolean
+	truncateDescription?: boolean
+}) => {
 	const serverURL = useServerUrl()
 
 	return (
@@ -48,9 +77,11 @@ const Achievement = ({achievement, isLocked}: {achievement: RetroAchievement; is
 					width={40}
 					height={40}
 				/>
-				<span className='text-neutral-300 font-medium text-sm'>{achievement.title}</span>
+				<span className='text-neutral-300 font-medium text-sm line-clamp-2'>{achievement.title}</span>
 			</div>
-			<span className='font-medium text-sm text-neutral-400'>{achievement.description}</span>
+			<span className={clsx('font-medium text-sm text-neutral-400', truncateDescription && 'line-clamp-2')}>
+				{achievement.description}
+			</span>
 		</div>
 	)
 }
--- src/views/rom/components/achievements.tsx
+++ src/views/rom/components/achievements.tsx
@@ -2,9 +2,6 @@ import HeroCard from '@/components/ui/hero-card'
 import {RetroAchievementsMetadata} from '@/models/retroachievements'
 import AchievementProgress from '@/components/ui/achievement-progress'
 
-const MAX_ITEMS_COLLAPSED = 2
-const MAX_ITEMS_LOCKED_COLLAPSED = 2
-
 export default function Achievements({rom}: {rom: {raId: number; mergedRaMetadata: RetroAchievementsMetadata}}) {
 	return (
 		<HeroCard
@@ -14,8 +11,7 @@ export default function Achievements({rom}: {rom: {raId: number; mergedRaMetadat
 				<AchievementProgress
 					contentClassName='flex flex-col gap-2 max-[1110px]:flex-row'
 					rom={rom}
-					maxLockedAchievementCount={MAX_ITEMS_LOCKED_COLLAPSED}
-					maxUnlockedAchievementCount={MAX_ITEMS_COLLAPSED}
+					maxAchievementCount={4}
 				/>
 			}
 			dialogComponent={<AchievementProgress contentClassName='grid grid-cols-3 gap-6' rom={rom} />}
